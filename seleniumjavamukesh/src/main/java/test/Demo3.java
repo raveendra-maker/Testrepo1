5ed9efe6c041f5e5cad7f8a450dd691bf09e219c
Raveendra <ravichintin@gmail.com>
2026-07-26 14:32:44 +0530

    added the demo3 in test package for testing

---
+++ seleniumjavamukesh/src/main/java/test/Demo3.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 5, 10, 15};
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
